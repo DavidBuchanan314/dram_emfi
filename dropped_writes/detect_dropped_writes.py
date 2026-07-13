1f0c0dbd7711e2f8700b3400c6980b49db52f3d5
David Buchanan <d@vidbuchanan.co.uk>
2026-07-13 22:22:30 +0100

    better write-drop detector

--- dropped_writes/detect_dropped_writes.py
+++ dropped_writes/detect_dropped_writes.py
@@ -1,22 +1,37 @@
 import os
 import mmap
 
-BUF_SIZE = 128 * 0x100000  # should ideally exceed LLC
+FILLER_SIZE = 0x10000  # smaller than LLC
+BUF_SIZE = 128 * 0x100000  # should exceed LLC
 CL_SIZE = 64
 
-a = os.urandom(BUF_SIZE)
-b = os.urandom(BUF_SIZE)
-buf = mmap.mmap(-1, len(a)) # mmap means we're page aligned (which means we're cache-line-aligned)
+def mmap_count(haystack: mmap.mmap, needle: bytes):
+	# for some reason mmap doesn't have .find
+	i = 0
+	count = 0
+	while i < len(haystack):
+		i = haystack.find(needle, i)
+		if i == -1:
+			break
+		i += len(needle)
+		count += 1
+	return count
+
+a = os.urandom(FILLER_SIZE)
+b = os.urandom(FILLER_SIZE)
+buf = mmap.mmap(-1, BUF_SIZE) # mmap means we're page aligned (which means we're cache-line-aligned)
 
 print(f"waiting for dropped writes at pid {os.getpid()}")
 
 while True:
 	a, b = b, a
-	buf[:] = a
-	if buf[:] != a:
+	for i in range(0, BUF_SIZE, FILLER_SIZE):
+		buf[i:i+FILLER_SIZE] = a
+	if mmap_count(buf, a) != BUF_SIZE // FILLER_SIZE:
 		print("inconsistency detected!")
-		for i in range(0, len(buf), CL_SIZE):
-			if buf[i:i+CL_SIZE] == b[i:i+CL_SIZE]:  # look for old value, specifically
+		for i in range(0, BUF_SIZE, CL_SIZE):
+			fi = i % FILLER_SIZE
+			if buf[i:i+CL_SIZE] == b[fi:fi+CL_SIZE]:  # look for old value, specifically
 				print(f"dropped write detected at offset", hex(i))
-			elif buf[i:i+CL_SIZE] != a[i:i+CL_SIZE]:  # some other type of fault
-				print(buf[i:i+CL_SIZE].hex(), "!=", a[i:i+CL_SIZE].hex(), "at offset", hex(i))
+			elif buf[i:i+CL_SIZE] != a[fi:fi+CL_SIZE]:  # some other type of fault
+				print(buf[i:i+CL_SIZE].hex(), "!=", a[fi:fi+CL_SIZE].hex(), "at offset", hex(i))
